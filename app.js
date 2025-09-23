7b9e2ec253364199328a1d2aff295e8acd18092c
Vaibhav Yadav <vaibhavyaduvanshi2503@gmail.com>
2025-09-23 14:06:46 +0530

    few adjutment

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080, ()=>{
-    console.log("server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
