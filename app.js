464afce00d3134da44b8b2bab5dceca3574ba3a7
Vaibhav Yadav <vaibhavyaduvanshi2503@gmail.com>
2026-06-22 00:36:11 +0530

    Remove connect-mongo

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -45,25 +44,20 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-    mongoUrl: process.env.ATLASDB_URL,
-    touchAfter: 24 * 3600,
-});
 
 store.on("error", (err) => {
     console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
-    store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    },
+    cookie:{
+        expires: Date.now() + 7*24*60*60*1000,
+        maxAge: 7*24*60*60*1000,
+        httpOnly:true
+    }
 };
 
 // app.get("/", (req,res)=>{
